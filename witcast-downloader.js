fb0c986775720f731628dba8dc154759e5a40075
Outhai SAIOUDOM <os55555gster@gmail.com>
2018-04-18 15:29:03 +0700

    Update witcast-downloader.js
    Fix Bug for main EP

--- witcast-downloader.js
+++ witcast-downloader.js
@@ -4,15 +4,22 @@
 
 var wget = require('node-wget');
 var i =1 ;
-var j =1;
+var j =0;
 while(i<=62){
     if(j<=4){
-        console.log('wget http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'-'+j+'.mp3');
-        wget('http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'-'+j+'.mp3');
+        
+       if(j==0){
+        wget('http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'.mp3');
+        console.log('wget http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'.mp3');
         j +=1;
+        }else{
+        wget('http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'-'+j+'.mp3');
+        console.log('wget http://media.blubrry.com/witcast/s/witcastthailand.com/witcast/witcast-ep'+i+'-'+j+'.mp3');
+        j +=1;
+        }
     }
     else{
         i +=1;
-        j =1;
+        j =0;
     }
   }
